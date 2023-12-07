b1f6b07b9901540dc04fecdb3d0b47c00048b786
Bárbara Lucia Lombardo <147457795+Fos7er365@users.noreply.github.com>
2023-12-07 01:15:29 -0300

    update: Commit 4th part (Quota exceeded).

---
+++ Assets/BackgroundMusicTriggerPlay.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackgroundMusicTriggerPlay : MonoBehaviour
+{
+    AudioManager audioMgr;
+    [SerializeField] string audioToPlay;
+    // Start is called before the first frame update
+    void Start()
+    {
+        audioMgr = GameObject.Find("AudioManager").GetComponent < AudioManager>() ;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "Player" && !audioMgr.GetAudio(audioToPlay).Source.isPlaying)
+            audioMgr.Play(audioToPlay);
+    }
+}
